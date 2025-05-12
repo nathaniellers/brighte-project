7d8bc08942790972b74bca81c2325523d66ddd12
nathaniellers <nathanielleromero18@gmail.com>
2025-05-12 22:18:46 +0800

    update frontend

---
+++ brighte-fe/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Navbar from './component/Navbar';
+import LeadList from './component/LeadList';
+import { LeadForm } from './component/LeadForm';
+
+
+function App() {
+  // Set up Apollo Client
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
+
+  const [showForm, setShowForm] = useState(false);
+
+  return (
+    <>
+      <Navbar/>
+      <ApolloProvider client={client}>
+        {showForm && (
+          <div className="modal-overlay" onClick={() => setShowForm(false)}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+              <LeadForm setShowForm={setShowForm}/>
+            </div>
+          </div>
+        )}
+        <LeadList setShowForm={setShowForm}/>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
